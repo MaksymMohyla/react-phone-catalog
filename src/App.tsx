529fe04122853effa6e2ace76375e4e096e75223
MaksymMohyla <greenparksquad@gmail.com>
2025-01-31 20:09:55 +0200

    created app, main and root

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+import { Outlet } from "react-router-dom";
+import { debounce } from "lodash";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { setLanguage, setScreenWidth } from "./features/globalSlice";
+import { Header, HeaderOrigin } from "./components/PageTopComponents/Header";
+import { Menu } from "./components/PageTopComponents/Menu";
+import { Footer } from "./components/Footer";
+import {
+  initDetailedProducts,
+  initProducts,
+  setCartList,
+  setFavoritesList,
+  setTotalItemsInCart,
+} from "./features/productSlice";
+import {
+  getProductsFromStorage,
+  LocaleStorageKeys,
+  saveProductsToStorage,
+} from "./app/localeStorage";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const dispatch = useAppDispatch();
+  const { favoritesList, cartList } = useAppSelector((st) => st.products);
+  const { language, isMenuOpened } = useAppSelector((st) => st.global);
+
+  //#region get items from server
+  /* get productList from server. I do this in App because this list
+  is needed in several components that are all child of App */
+  useEffect(() => {
+    dispatch(initProducts());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(initDetailedProducts());
+  }, [dispatch]);
+  //#endregion
+
+  // recalculate screenWidth everytime it changes
+  useEffect(() => {
+    const handleResize = debounce(() => {
+      dispatch(setScreenWidth(window.innerWidth));
+    }, 200);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [dispatch]);
+
+  //#region work with storage
+  useEffect(() => {
+    if (localStorage.getItem(LocaleStorageKeys.FAVORITES)) {
+      dispatch(
+        setFavoritesList(getProductsFromStorage(LocaleStorageKeys.FAVORITES))
+      );
+    }
+
+    if (localStorage.getItem(LocaleStorageKeys.CART)) {
+      dispatch(setCartList(getProductsFromStorage(LocaleStorageKeys.CART)));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    saveProductsToStorage(LocaleStorageKeys.FAVORITES, favoritesList);
+  }, [favoritesList]);
+
+  useEffect(() => {
+    saveProductsToStorage(LocaleStorageKeys.CART, cartList);
+
+    dispatch(
+      setTotalItemsInCart(
+        cartList.reduce((acc, curr) => {
+          return (curr.quantity || 1) + acc;
+        }, 0)
+      )
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cartList]);
+
+  useEffect(() => {
+    if (localStorage.getItem(LocaleStorageKeys.LANGUAGE)) {
+      dispatch(setLanguage(localStorage.getItem(LocaleStorageKeys.LANGUAGE)));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    localStorage.setItem(LocaleStorageKeys.LANGUAGE, language);
+  }, [language]);
+  //#endregion
+
+  useEffect(() => {
+    if (isMenuOpened) {
+      window.document.documentElement.style.overflowY = "hidden";
+    } else {
+      window.document.documentElement.style.overflowY = "auto";
+    }
+  }, [isMenuOpened]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <Header origin={HeaderOrigin.ONPAGE} />
+      <Menu />
+      <div className="outlet-container">
+        <Outlet />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
